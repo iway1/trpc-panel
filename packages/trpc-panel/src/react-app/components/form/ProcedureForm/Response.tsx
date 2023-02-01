7b6ff1ea464a2c33e0677201370baa744b88c876
Isaac Way <iway1@vols.utk.edu>
2023-02-01 13:21:13 -0600

    Now wraps response lines if they're too long for container

--- packages/trpc-panel/src/react-app/components/form/ProcedureForm/Response.tsx
+++ packages/trpc-panel/src/react-app/components/form/ProcedureForm/Response.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { FormSection } from "./FormSection";
 
 export function Response({ children }: { children: string }) {
-    return (
-        <FormSection title="Response">
-            <p className="whitespace-pre font-mono">{children}</p>
-        </FormSection>
-    );
+  return (
+    <FormSection title="Response">
+      <p className="font-mono whitespace-pre-wrap break-words">{children}</p>
+    </FormSection>
+  );
 }
