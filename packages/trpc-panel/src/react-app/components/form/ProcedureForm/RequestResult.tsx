11cceaa02c6ddb241f1ccccbd976fbf0443d4836
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-02-06 14:47:43 -0500

    feat: use collapsable JSON viewer

--- packages/trpc-panel/src/react-app/components/form/ProcedureForm/RequestResult.tsx
+++ packages/trpc-panel/src/react-app/components/form/ProcedureForm/RequestResult.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Response } from "./Response";
-import json from "json-bigint";
+
 
 export function RequestResult({ result }: { result: any }) {
-  return <Response>{`${json.stringify(result, null, 2)}`}</Response>;
+  return <Response>{result}</Response>;
 }
