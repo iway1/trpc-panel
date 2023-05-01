66b48bc6dcc0e8dc98ad620924059c12d97d0f05
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-05-01 16:42:42 -0400

    update json-viewer, add response size & time

--- packages/trpc-panel/src/react-app/components/form/ProcedureForm/RequestResult.tsx
+++ packages/trpc-panel/src/react-app/components/form/ProcedureForm/RequestResult.tsx
@@ -2,6 +2,18 @@ import React from "react";
 import { Response } from "./Response";
 import json from "json-bigint";
 
-export function RequestResult({ result }: { result: any }) {
-  return <Response>{result}</Response>;
+export function RequestResult({
+  result,
+  size,
+  time,
+}: {
+  result: any;
+  size?: number;
+  time?: number;
+}) {
+  return (
+    <Response size={size} time={time}>
+      {result}
+    </Response>
+  );
 }
